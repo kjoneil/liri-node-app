2986b5ab8d87ff68f593cb48bd5be41bfaa5e3f1
kjoneil <kjoneil.rw@gmail.com>
2019-03-27 00:16:30 -0500

    Managed to almost get Liri to process the readfile into the function movie

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # liri-node-app
-This will search Spotify for songs and OMBD for movies
+This will search 'Spotify' for songs, 'OMBD' for movie information and 'Band in Town' for venue information on bands. It will also take in written commands from a random text file.
--- liri.js
+++ liri.js
@@ -2,8 +2,10 @@
 // var keys = require("./keys.js");
 
 // var spotify = new Spotify(keys.spotify);
+// function spotify(){
 
-// BAND IN TOWN PART OF APP
+// }
+
 var axios = require("axios")
 var action = process.argv[2];
 
@@ -15,7 +17,13 @@ switch (action){
     case "concert-this":
     music();
     break;
+
+    case "do-what":
+    doWhat();
+    break;
 }
+
+// BAND IN TOWN FUNCTION
 function music(){
 var artistSearch = process.argv[3];
 
@@ -23,33 +31,20 @@ var artistName = "";
 for (var i = 0; i < artistSearch.length; i++) {
     if (i > 30 && i < artistSearch.length) {
      artistName = artistName + "+" + artistSearch[i];
-       }
-       else {
+       } else {
      artistName += artistSearch[i];
    }
 }
-var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
     console.log(queryUrl);
-axios.get(queryUrl).then(function(response){
-    console.log("Name of Venue: "+ response.data[0].venue.name);
-    console.log("City: "+ response.data[0].venue.city);
-    console.log("Date of the Event: "+ response.data[0].datetime);
-
- 
-    }
+        axios.get(queryUrl).then(function(response){
+            console.log("Name of Venue: "+ response.data[0].venue.name);
+            console.log("City: "+ response.data[0].venue.city);
+            console.log("Date of the Event: "+ response.data[0].datetime);
+        }
 );
 }
-
-// OMBD SEARCH
-// var axios = require("axios");
-
-// var action = process.argv[2];
-
-// switch (action){
-//     case "movie-this":
-//     movie();
-//     break;
-// }
+// OMDB FUNCTION
 function movie(){
 var movieSearch = process.argv[3];
 
@@ -58,37 +53,35 @@ var movieName = "";
 for (var i = 0; i < movieSearch.length; i++) {
     if (i > 30 && i < movieSearch.length) {
      movieName = movieName + "+" + movieSearch[i];
-       }
-       else {
+       } else {
      movieName += movieSearch[i];
    }
 }
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    console.log(queryUrl);
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        console.log(queryUrl);
     
-axios.get(queryUrl).then(function(response){
-        
-    console.log("Movie: "+response.data.Title);
-    console.log("Year: "+response.data.Released);
-    console.log("IMBD: "+response.data.imdbRating);
-    console.log("Rotten Tomatoes :"+response.data.Ratings[1].Value);
-    console.log("Country: "+response.data.Country);
-    console.log("Language: "+response.data.Language);
-    console.log("Plot: "+response.data.Plot);
-    console.log("Actors: "+response.data.Actors);
-    }       
-);
+        axios.get(queryUrl).then(function(response){
+            console.log("Movie: "+response.data.Title);
+            console.log("Year: "+response.data.Released);
+            console.log("IMBD: "+response.data.imdbRating);
+            console.log("Rotten Tomatoes :"+response.data.Ratings[1].Value);
+            console.log("Country: "+response.data.Country);
+            console.log("Language: "+response.data.Language);
+            console.log("Plot: "+response.data.Plot);
+            console.log("Actors: "+response.data.Actors);
+        }       
+    );
 }
-
 // DO WHAT LIRI SAYS
-// var fs = require("fs");
-
-// fs.readFile("random.txt", "utf8", function(err, text){
-//     if(err){
-//         return console.log(err);
-//     }
-//     var output = text.split(",");
-//     console.log(output);
+function doWhat(){
+var fs = require("fs");
+fs.readFile("random.txt", "utf8", function(err, data){
+    if(err){
+        return console.log(err);
+    }
+    var output = data.split(",");
+    console.log(output);
     
-// })
+});
+}
 
